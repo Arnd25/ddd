10347e663da40c4324d07165e44b2d80e49690e6
Arnd25 <And30206@gmail.com>
2025-10-29 15:07:31 +0300

    Update NewscardProps.ts

--- src/widgets/news/Ui/NewscardProps.ts
+++ src/widgets/news/Ui/NewscardProps.ts
@@ -9,13 +9,13 @@ interface NewscardProps {
 }
 export const NewscardProps: NewscardProps[] = [
     {
-        image: "../../../../public/static/a4def80ea7cfddba6d84ea608cab5fd3.jpg",
+        image: Img,
         title: "шанс выиграть призы",
         date: "04.10.2025",
         href: "",
     },
     {
-        image: "../../../../public/static/a4def80ea7cfddba6d84ea608cab5fd3.jpg",
+        image: Img,
         title: "шанс выиграть призы",
         date: "04.10.2025",
         href: "",
