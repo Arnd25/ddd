367fbdeb18bc9b3847ec254df28acb397e1564aa
Arnd25 <And30206@gmail.com>
2025-10-29 14:13:22 +0300

    Update menu.data.ts

--- src/data/menu.data.ts
+++ src/data/menu.data.ts
@@ -1,1 +1,23 @@
+import {Menu} from "@/types/menu.interface"
 
+
+
+export const MENU_DATA: Menu[] = [
+    {
+        label:"Афиша",
+        href: "#posters",
+    },
+    {
+        label:"Скоро в кино",
+        href: "#soon",
+    },
+    {
+        label:"Акции и скидки",
+        href: "#discounts",
+    },
+    {
+        label:"Новости",
+        href: "#news"
+    },
+
+];
