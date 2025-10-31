96d937fee4d96661def1b3411edd60fba9084a71
Arnd25 <And30206@gmail.com>
2025-11-01 00:42:55 +0300

    Update section.ui.tsx

--- src/widgets/hero/ui/section.ui.tsx
+++ src/widgets/hero/ui/section.ui.tsx
@@ -1,69 +1,31 @@
-import {cn} from "@/lib/utils";
+
+import { cn } from "@/lib/utils";
 import Container from "@/components/shared/container";
+
 const backgroundImageUrl = `${process.env.PUBLIC_URL}/static/background.png`;
 
-
-
-
-
-
 interface Props {
-    className?: string;
+  className?: string;
 }
 
 export const SectionUi: React.FC<Props> = ({ className }) => {
-    return(
-        <section  className="bg-cover bg-center" style={{ backgroundImage: `url(${backgroundImageUrl})` }}
-            >
-            <Container className="w-full ">
-                <div className="gap-10   min-h-[700] pt-56">
-                    <h1 className="text-white text-5xl mb-12">Кино, которое объединяет</h1>
-                    <p className="text-gray-400 max-w-md text-xl">Мощный звук, кристально чёткое изображение, удобные кресла и аромат свежего попкорна — всё создано, чтобы вы забыли о времени и окунулись в историю.</p>
-                </div>
-            </Container>
-        </section>
-    );
+  return (
+    <section
+      className={cn(className, "bg-cover bg-center")}
+      style={{
+        backgroundImage: `url(${backgroundImageUrl})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center"
+      }}
+    >
+      <Container className="w-full">
+        <div className="gap-10 min-h-[700] pt-56">
+          <h1 className="text-white text-5xl mb-12">Кино, которое объединяет</h1>
+          <p className="text-gray-400 max-w-md text-xl">
+            Мощный звук, кристально чёткое изображение, удобные кресла и аромат свежего попкорна — всё создано, чтобы вы забыли о времени и окунулись в историю.
+          </p>
+        </div>
+      </Container>
+    </section>
+  );
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
